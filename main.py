78a6e2bbf92b024fc6623f9f49ea748b416a178b
Caio César <br.caiorecife@cesarleitao.com.br>
2020-12-16 15:28:22 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,82 @@
+# Caio César Melquiades Leitão 2020
+
+import math
+# algoritmo de equação de 1 grau (em python)
+
+#1) (reposta de nao calculo)
+# A
+
+#2) (reposta de nao calculo)
+# C
+
+#3) (reposta de nao calculo)
+# B
+
+#4)
+# D
+
+# 5)
+# C
+
+# 6)
+print('Numa cidade da Argentina a temperatura era de 12c. Cinco horas depois, o termometro registrou -7c');
+q6n1 = float(input('Digite o a temperatura registrada: '))
+q6n2 = float(input('Digite a temperatura registada cinco horas depois: '))
+
+q6s = float(q6n1 - q6n2)
+print('A variação da temperatura nessa cidade foi: ', q6s)
+# a resposta certa é D
+
+# 7)
+q7raiz1 = float(input('Digite a raiz: '))
+q7raiz2 = float(input('Digite a raiz: '))
+q7raizQ1 = q7raiz1 ** (1/2)
+q7raizQ2 = q7raiz2 ** (1/2)
+q7s = (q7raizQ1 * q7raizQ2)
+print(q7s)
+
+#8)
+print('area do primeiro quadrado')
+q8n1 = float(input('digite o lado 1: '))
+q8n2 = float(input('digite o lado 2: '))
+print('area do segundo quadrado')
+q8n3 = float(input('digite o lado 1: '))
+q8n4 = float(input('digite o lado 2: '))
+print('area do primeiro triangulo')
+q8n5 = float(input('digite o lado 1: '))
+q8n6 = float(input('digite o lado 2: '))
+
+q8s1 = q8n1 * q8n2
+q8s2 = q8n3 * q8n4
+q8s3 = ((q8n5 * q8n6) / 2)
+
+q8s4 = (q8s1 + q8s2 + q8s3)
+
+print('a area é: ', q8s4)
+
+# reposta certa é 11
+
+# 9)
+#B ou D
+
+# 10) calculo de produto notavel
+q10raiz1 = float(input('Digite a primeira raiz: '))
+q10raiz2 = float(input('Digite a segunda raiz'))
+
+# 11)
+print('valores 1')
+q11n1 = float(input('numero da base 1:'))
+print('valores 2')
+q11n3 = float(input('numero da base 3'))
+q11n4 = float(input('numero vertical 4'))
+
+#regra de 3 para descubrir o x
+q11r3 = ((q11n1 * q11n4) / q11n3)
+print(q11r3)
+
+#reposta certa B
+
+# 12) teorema de pitagoras
+
+
+
